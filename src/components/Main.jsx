042c5e120c46a8e501fe92f6bd9ccc3717cc743d
YuriShido <sydonce57@gmail.com>
2021-04-22 11:51:06 -0700

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react' 
+import styled from 'styled-components'
+
+import Sidebar from './SideBar';
+//chat component
+
+const MainContainer = styled.div`
+    display: flex
+`
+
+const Main = () => (
+    <MainContainer>
+        <Sidebar />
+    </MainContainer>
+)
+
+export default Main
